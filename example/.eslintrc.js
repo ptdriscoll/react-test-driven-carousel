fc27ba946d4eebf150904b983308e9f30d2b172a
Patrick Driscoll <pdriscoll@satx.rr.com>
2022-02-09 19:17:55 -0600

    :bulb: Add example page

---
+++ example/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: {
     carousel: './src/Carousel.js',
+    example: './example/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Carousel Example', //1
+      chunks: ['example'], //2
+    }),
+  ],
   module: {
     rules: [
       {
