930985d400ab13dda705c87c06dca0094aa6357e
Patrick Driscoll <pdriscoll@satx.rr.com>
2022-02-09 18:05:52 -0600

    :wrench: Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'development',
+  entry: {
+    carousel: './src/Carousel.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: require.resolve('babel-loader'),
+      },
+    ],
+  },
+};
