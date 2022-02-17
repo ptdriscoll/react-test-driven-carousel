5d2e5668bc45a81dd9dc474a0796fe8091f02dbd
Patrick Driscoll <pdriscoll@satx.rr.com>
2022-02-16 20:47:59 -0600

    :wrench: Initial Storybook setup

---
+++ stories/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Carousel from '../src/Carousel';
+import slides from '../example/slides';
+
+storiesOf('Carousel', module).add('default', () => (
+  <Carousel slides={slides} />
+));
