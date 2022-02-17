3d5c6b4de681a85e2573b1f841492d901ee4f5c8
Patrick Driscoll <pdriscoll@satx.rr.com>
2022-02-16 22:26:36 -0600

    :memo: Build storybook docs

---
+++ docs/main.2f8f6fbed6d6fe1dfcf3.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(414),c(547),n.exports=c(824)},1:function(n,o){},547:function(n,o,c){"use strict";c.r(o);c(548),c(751)}},[[0,1,2]]]);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Carousel from '../src/Carousel';
 import slides from '../example/slides';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, number } from '@storybook/addon-knobs';
 
 storiesOf('Carousel', module).add('default', () => (
-  <Carousel slides={slides} />
+  <Carousel slides={slides} onIndexChange={action('onIndexChange')} />
+));
+
+const stories = storiesOf('Carousel', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('default', () => (
+  <Carousel
+    autoAdvanceDelay={number('autoAdvanceDelay', 10e3)}
+    slides={slides}
+    onIndexChange={action('onIndexChange')}
+  />
 ));
